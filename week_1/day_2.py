8dce67c62d34a10fd2b896086ed54bb12e666f7d
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-02 12:22:33 +0100

    day one

---
+++ week_1/day_2.py
@@ -0,0 +1,25 @@
+# This is a single line comment used for documentation.
+
+'''
+This is a multi-line comment
+   that spans multiple lines
+    and is often used for documentation.
+'''
+
+# Variables and Data Types
+bottle = "water"
+
+# data types
+
+"qwerty1234567890@#$%^&*()_+"  # string
+
+1234567890  # integer
+
+123.4567890  # float
+
+True  # boolean
+False  # boolean
+
+first_name = "Cosmos"  # snake case
+
+print(True)  # print function to display output
