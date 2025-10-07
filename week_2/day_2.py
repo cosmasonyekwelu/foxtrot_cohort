8206396f3ed18aa7b3e7207f4e49be227b438a0b
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-07 11:34:06 +0100

    push week2.

--- week_2/day_2.py
+++ week_2/day_2.py
@@ -35,3 +35,30 @@ print("Greater Than and Equal To: ", greater_than_and_equal_to)
 # Less Than and Equal To
 less_than_and_equal_to = stored_value <= search_input
 print("Less Than and Equal To: ", less_than_and_equal_to)
+
+
+# -------Logical Operators in Python-------
+''''
+ Logical operators are used to make more than one comparison.
+There are three logical operators in Python: AND, OR, NOT.
+AND - Operator-checks if both the comparisons are true.
+OR - Operator-checks if at least one of the comparisons is true.
+NOT - Operator-reverses the result of the comparison.(Negative operator: makes a value the opposite)
+'''
+age = 20
+nyse = "done"
+# AND Operator
+is_all_true = age >= 20 and nyse == "done"
+print("AND: ", is_all_true)
+
+# OR Operator
+is_one_true = age >= 20 or nyse == "in_transit"
+print("OR: ", is_one_true)
+
+# NOT Operator : gives the opposite value
+is_not_true = not(age >= 20 and nyse == "done")
+print("NOT: ", is_not_true)
+
+value = False
+negate_value = not value
+print("Not: ", negate_value)
