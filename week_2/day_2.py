20cb74e6cb0568780971d7a0cd2f64814c0887ff
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-07 11:01:31 +0100

    push week2

---
+++ week_2/day_2.py
@@ -0,0 +1,37 @@
+# Comparison Operators in Python
+# Comparison operators are used to compare two data or values.
+
+'''
+== Is Equal
+!= Not Equal
+> Greater Than
+<  Less Than
+>= Greater Than and Equal To
+<= Less Than and Equal To
+'''
+
+stored_value = 10
+search_input = 4
+# Is Equal
+is_equal = stored_value == search_input
+print("Is Equal: ", is_equal)
+
+# Not Equal
+not_equal = stored_value != search_input
+print("Not Equal to: ", not_equal)
+
+# Greater Than
+greater_than = stored_value > search_input
+print("Greater Than: ", greater_than)
+
+# Less Than
+less_than = stored_value < search_input
+print("Less Than: ", less_than)
+
+# Greater Than and Equal To
+greater_than_and_equal_to = stored_value >= search_input
+print("Greater Than and Equal To: ", greater_than_and_equal_to)
+
+# Less Than and Equal To
+less_than_and_equal_to = stored_value <= search_input
+print("Less Than and Equal To: ", less_than_and_equal_to)
