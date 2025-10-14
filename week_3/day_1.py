bfadb684e2ab10fb4f43c01caf5f516b44fd5086
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-14 14:55:32 +0100

    week3 day 1 exercise.

--- week_3/day_1.py
+++ week_3/day_1.py
@@ -1,6 +1,7 @@
 # ========CONTROL STRUCTURE======
 
-# Conditions
+# Conditions: Conditional statements in Python are used to execute certain blocks of code based on specific conditions.
+# These statements help control the flow of a program, making it behave differently in different situations.
 
 # If statement
 
