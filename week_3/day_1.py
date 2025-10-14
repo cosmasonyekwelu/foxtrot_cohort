cf0ce9a0aabf9b97ae87b7680b0e4607c0d1abb2
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-14 11:43:00 +0100

    week 3 day 1.

--- week_3/day_1.py
+++ week_3/day_1.py
@@ -17,8 +17,16 @@
 
 destination_fee = 1000
 transport_fee = 900
+train = "available"
 
-if transport_fee <= destination_fee:
+# if transport_fee <= destination_fee:
+#     print(f"{"==" * 24}\nGetting to destination Successful.\n{"==" * 24}")
+# else:
+#     print(f"{"==" * 24}\nGetting to destination Unsuccessful.\n{"==" * 24}")
+
+if transport_fee <= destination_fee and train == "available":
     print(f"{"==" * 24}\nGetting to destination Successful.\n{"==" * 24}")
+elif transport_fee <= destination_fee and train != "available":
+    print(f"{"==" * 24}\nTrain Not Available.\n{"==" * 24}")
 else:
     print(f"{"==" * 24}\nGetting to destination Unsuccessful.\n{"==" * 24}")
