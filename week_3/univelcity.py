ffb1c80b80ca71904eedc9777a4de2023b0fd009
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-14 12:22:04 +0100

    week3 day 1 file.

--- week_3/univelcity.py
+++ week_3/univelcity.py
@@ -1,2 +1,11 @@
-input_value = input()
-print("The value i wrote in my terminal is:",input_value)
+# input_value = input()
+# print("The value i wrote in my terminal is:",input_value)
+
+ui_ux_design = []
+frontend_development = []
+backend_development = []
+
+print("Welcome to Univelcity")
+name = input("What is your name")
+course = input(
+    f"What course are you planning to take?\n1. UI/UX design.\n2. Frontend Development\n3. Backend Development")
