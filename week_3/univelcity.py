28cebd4be53798d7564179ed22500d629b40510e
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-14 12:50:55 +0100

    week3 day 1 file...

--- week_3/univelcity.py
+++ week_3/univelcity.py
@@ -8,4 +8,16 @@ backend_development = []
 print("Welcome to Univelcity")
 name = input("What is your name")
 course = input(
-    f"What course are you planning to take?\n1. UI/UX design.\n2. Frontend Development\n3. Backend Development")
+    f"What course are you planning to take?\n1. UI/UX design.\n2. Frontend Development\n3. Backend Development\n(Choose one 1/2/3): ")
+
+if course == "1":
+    ui_ux_design.append(name)
+elif course == "2":
+    frontend_development.append(name)
+elif course == "3":
+    backend_development.append(name)
+else:
+    print("You choses the wrong option. You are to choose between 1, 2 or 3.")
+    print("UI/UX Design:", ui_ux_design)
+    print("Frontend Development:", frontend_development)
+    print("Backend Development:", backend_development)
