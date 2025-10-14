be8505cc9e85e676b961526d6c81baadcd452b55
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-14 12:12:56 +0100

    week 3 day 1..

--- week_3/day_1.py
+++ week_3/day_1.py
@@ -16,17 +16,30 @@
 
 
 destination_fee = 1000
-transport_fee = 900
-train = "available"
+transport_fee = 1900
+train = "unavailable"
 
 # if transport_fee <= destination_fee:
 #     print(f"{"==" * 24}\nGetting to destination Successful.\n{"==" * 24}")
 # else:
 #     print(f"{"==" * 24}\nGetting to destination Unsuccessful.\n{"==" * 24}")
 
-if transport_fee <= destination_fee and train == "available":
-    print(f"{"==" * 24}\nGetting to destination Successful.\n{"==" * 24}")
-elif transport_fee <= destination_fee and train != "available":
-    print(f"{"==" * 24}\nTrain Not Available.\n{"==" * 24}")
-else:
-    print(f"{"==" * 24}\nGetting to destination Unsuccessful.\n{"==" * 24}")
+# if transport_fee <= destination_fee and train == "available":
+#     print(f"{"==" * 24}\nGetting to destination Successful.\n{"==" * 24}")
+# elif transport_fee <= destination_fee and train != "available":
+#     print(f"{"==" * 24}\nTrain Not Available.\n{"==" * 24}")
+# else:
+#     print(f"{"==" * 24}\nGetting to destination Unsuccessful.\n{"==" * 24}")
+
+# if False:
+# print("road a")
+# elif False:
+# print("train a")
+# elif True:
+# print("train b")
+# elif True:
+# print("train c")
+# elif True:
+# print("train d")
+# else False:
+# print("road b")
---
+++ week_3/univelcity.py
@@ -0,0 +1,2 @@
+input_value = input()
+print("The value i wrote in my terminal is:",input_value)
