7fb338b9a72bea293cee243d3ca724221f7c37ac
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-16 12:41:44 +0100

    week 3 next task

--- week_3/day_2.py
+++ week_3/day_2.py
@@ -5,46 +5,55 @@
 # In Python, a while loop is used to execute a block of statements repeatedly until a given condition is satisfied.
 # When the condition becomes false,
 # the line immediately after the loop in the program is executed.
-
-# num = 0
-# while num < 10:
-# print(num)
-# num = num + 1
+# NB: multiple line comment is used for block of codes
+"""
+num = 0
+while num < 10:
+print(num)
+num = num + 1
+"""
 
 # Infinite While Loop
 # If we want a block of code to execute infinite number of times
 # then we can use the while loop in Python to do so.
-# while (True):
-# print("Hello World!")
+"""
+while (True):
+print("Hello World!")
+"""
 
 #     For Loop
 # For loops is used to iterate over a sequence such as a list, tuple, string or range.
 # It allow to execute a block of code repeatedly, once for each item in the sequence.
 #
-# people = ["John", "Peter", "Ade", "Oluadamilare", "King"]
+"""
+people = ["John", "Peter", "Ade", "Oluadamilare", "King"]
 
-# # for person in people:
-# #     print(person)
-
-# searched_name = input("Who are you looking for")
-# for person in people:
-#     if person == searched_name:
-#         print(f"Person found: {person}")
-#         break # Stops the loop
-# else: #Runs the loop has stopped
-#     print("The person is not found.")
-
+for person in people:
+    print(person)
+    
+searched_name = input("Who are you looking for")
+for person in people:
+    if person == searched_name:
+        print(f"Person found: {person}")
+        break # Stops the loop
+else: #Runs the loop has stopped
+    print("The person is not found.")
+"""
+# Classwork
+"""
 first_num = [1, 2, 3, 4, 5]
 second_num = [6, 7, 8, 9, 10]
 
-# answer : [1,2,3,4,5,6,7,8,9,10]
-# hint use append()
+answer : [1,2,3,4,5,6,7,8,9,10]
+hint use append()
 
-# for num in second_num:
-#     first_num.append(num)
+for num in second_num:
+    first_num.append(num)
 
-# print(first_num)
-
+print(first_num)
+"""
+# Next  task
+"""
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 updated = []
 for num in numbers:
@@ -53,3 +62,15 @@ for num in numbers:
 else:
     print("Numbers:", numbers)
     print("Updated Numbers:", updated)
+"""
+
+# Next task
+
+people = ["John", "Peter", "Ade", "Oluadamilare", "King"]
+email_users = []
+
+for person in people:
+    email_address = person + "@mail.com"
+    email_users.append(email_address)
+else:
+    print(email_users)
