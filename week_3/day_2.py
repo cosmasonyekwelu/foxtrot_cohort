35de12c33c313eea18d0a2f1bfab846db0b6bb89
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-10-16 12:03:00 +0100

    week 3 For loop

--- week_3/day_2.py
+++ week_3/day_2.py
@@ -6,13 +6,30 @@
 # When the condition becomes false,
 # the line immediately after the loop in the program is executed.
 
-num = 0
-while num < 10:
-    print(num)
-    num = num + 1
+# num = 0
+# while num < 10:
+# print(num)
+# num = num + 1
 
 # Infinite While Loop
 # If we want a block of code to execute infinite number of times
 # then we can use the while loop in Python to do so.
-while (True):
-    print("Hello World!")
+# while (True):
+# print("Hello World!")
+
+#     For Loop
+# For loops is used to iterate over a sequence such as a list, tuple, string or range.
+# It allow to execute a block of code repeatedly, once for each item in the sequence.
+#
+people = ["John", "Peter", "Ade", "Oluadamilare", "King"]
+
+# for person in people:
+#     print(person)
+
+searched_name = input("Who are you looking for")
+for person in people:
+    if person == searched_name:
+        print(f"Person found: {person}")
+        break
+else:
+    print("The person is not found.")
