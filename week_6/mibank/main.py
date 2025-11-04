ab5abe39fbac7ff657a47ae6f5a00fb78c984122
Cosmas Onyekwelu <dpositiveworld@gmail.com>
2025-11-04 11:17:44 +0000

    week 6 task

---
+++ week_6/mibank/main.py
@@ -0,0 +1,32 @@
+
+
+class Main:
+    def __init__(self, name, founded):
+        self.name = name
+        self.founded = founded
+
+    def run(self):
+        print(
+            f"{'~' * 24}\n Welcome to {self.name} (Founded {self.founded})\n{'~' * 24}")
+
+        while True:
+            option = input(
+                "1. Create an Account\n2. Withdraw Money\n3. View Balance\n4. Exit\nChoose an option (1-4): ")
+
+            match option:
+                case "1":
+                    print("Account creation selected.")
+                case "2":
+                    print("Withdrawal option selected.")
+                case "3":
+                    print("Viewing balance...")
+                case "4":
+                    print("Exiting... Thank you for banking with us!")
+                    break
+                case _:
+                    print("Invalid option. Please choose a number between 1 and 4.")
+
+
+if __name__ == "__main__":
+    main = Main("MiBank", 2025)
+    main.run()
---
+++ week_6/z.py
@@ -0,0 +1,34 @@
+import json
+
+# x = '{"name": "Alice", "age": 30, "city": "New York"}'
+
+
+# to_dict = json.loads(x)
+
+
+# open ("store.txt", "r")
+
+# File Handling
+# The key function for working with files in Python is the open() function.
+
+# The open() function takes two parameters; filename, and mode.
+
+# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+
+# "a" - Append - Opens a file for appending, creates the file if it does not exist
+
+# "w" - Write - Opens a file for writing, creates the file if it does not exist
+
+# "x" - Create - Creates the specified file, returns an error if the file ex
+
+# In addition you can specify if the file should be handled as binary or text mode
+
+# "t" - Text - Default value. Text mode
+
+# "b" - Binary - Binary mode (e.g. images)
+
+x = {"name": "Alice", "age": 30, "city": "New York"}
+
+with open("store.json", "w") as store:
+    convert_to_json = json.dumps(x)
+    store.write(convert_to_json)
